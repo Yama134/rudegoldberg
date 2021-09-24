15fee4a51e66ebd862a780c7f88b72c39b827873
Yama134 <91343895+Yama134@users.noreply.github.com>
2021-09-24 11:32:00 -0700

    Final Project

---
+++ rudegoldberg/Assets/Scripts/SpinnerXAxis.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinnerXAxis : MonoBehaviour
+{
+    public GameObject gameObject;
+    public float spinRate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //gameObject = GetComponent<GameObject>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        gameObject.transform.Rotate(spinRate, 0.0f, 0.0f);
+    }
+}
